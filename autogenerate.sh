98f79ca2783adad16b2d35d0a0791b304f7500a6
Shane Mendez <mendezsoftware@gmail.com>
2021-12-20 15:23:04 -0600

    fix pls

--- autogenerate.sh
+++ autogenerate.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
 
+
+## this doesn't handle unknown parameters well
+
 #cd ./whitepapers
 #sh generate.sh
 #echo "Whitepaper generation complete"
 #cd ..
-sass templates/style/scss/custom-bootstrap.scss build/css/custom-bootstrap.css
-pug templates/routes
-echo "SASS compilation complete"
+
+
+
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -s|--sass) 
+        sass templates/style/scss/custom-bootstrap.scss build/css/custom-bootstrap.css
+        echo "SASS compilation complete"
+        shift ;;
+        -p|--pug) 
+        pug templates/routes/pug -o templates/routes/ 
+        shift ;;
+
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+done
+        
+   
 if [ -z "`raco pkg show | grep yaml`" ]
 then
     raco pkg install yaml
+    echo "Packages installed"
 fi
-echo "Packages installed"
+
 racket generate.rkt
--- generate.rkt
+++ generate.rkt
@@ -4,43 +4,43 @@
          (for-syntax racket/string))
 (require "l10n.rkt")
 
-(define-macro (generate fname)
-  `(let ([FILENAME, fname])
-     (with-output-to-file (string-append "build/" (if (equal? LANG "en-US")
-                              ,fname
-                              (string-replace ,fname ".html"
-                                              (format "-~a.html"
-                                                      (string-downcase (string-replace LANG "-" ""))))))
-       #:exists 'replace
-       (lambda ()
-         (display (include-template , (string-append 
-         "templates/routes/" (string-replace fname ".html" ".src.html"))))))))
+;;; (define-macro (generate fname)
+;;;   `(let ([FILENAME, fname])
+;;;      (with-output-to-file (string-append "build/" (if (equal? LANG "en-US")
+;;;                               ,fname
+;;;                               (string-replace ,fname ".html"
+;;;                                               (format "-~a.html"
+;;;                                                       (string-downcase (string-replace LANG "-" ""))))))
+;;;        #:exists 'replace
+;;;        (lambda ()
+;;;          (display (include-template , (string-append 
+;;;          "templates/routes/" (string-replace fname ".html" ".src.html"))))))))
 
-(define-macro (generate-all fname)
-  `(let ()
-     (generate ,fname)
-     (let ([LANG "zh-CN"])
-       (generate ,fname))
-     (let ([LANG "zh-HK"])
-       (generate ,fname))))
+;;; (define-macro (generate-all fname)
+;;;   `(let ()
+;;;      (generate ,fname)
+;;;     ;;;  (let ([LANG "zh-CN"])
+;;;     ;;;    (generate ,fname))
+;;;     ;;;  (let ([LANG "zh-HK"])
+;;;     ;;;    (generate ,fname))))
+;;;   ))
+;;; (define LANG "en-US")
 
-(define LANG "en-US")
+;;; (define-macro (l10n str)
+;;;   `(l10n-lookup ,str LANG))
 
-(define-macro (l10n str)
-  `(l10n-lookup ,str LANG))
+;;; (define-macro (l10n-url str)
+;;;   (define lang-suffix (gensym 'lsuf))
+;;;   `(if (equal? LANG "en-US") ,str
+;;;        (let ([,lang-suffix (string-downcase (string-replace LANG "-" ""))])
+;;;          (string-replace ,str ".html"
+;;;                          (format "-~a.html" ,lang-suffix)))))
 
-(define-macro (l10n-url str)
-  (define lang-suffix (gensym 'lsuf))
-  `(if (equal? LANG "en-US") ,str
-       (let ([,lang-suffix (string-downcase (string-replace LANG "-" ""))])
-         (string-replace ,str ".html"
-                         (format "-~a.html" ,lang-suffix)))))
+;;; (define-macro (l10n-url-foreign str lang)
+;;;   `(let ([LANG ,lang])
+;;;      (l10n-url ,str)))
 
-(define-macro (l10n-url-foreign str lang)
-  `(let ([LANG ,lang])
-     (l10n-url ,str)))
-
-(generate-all "learn.html")
-;;; (generate-all "resources.html")
-;;; (generate-all "roadmap.html")
-(displayln "HTML compilation complete")
+;;; (generate-all "learn.html")
+;;; ;;; (generate-all "resources.html")
+;;; ;;; (generate-all "roadmap.html")
+;;; (displayln "HTML compilation complete") 
