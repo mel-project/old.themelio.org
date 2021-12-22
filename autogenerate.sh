a119715f9163d7d457bd28db2a66c6c5b7344c6f
Shane Mendez <mendezsoftware@gmail.com>
2021-12-22 14:53:04 -0600

    added new pages

--- autogenerate.sh
+++ autogenerate.sh
@@ -8,23 +8,34 @@
 #echo "Whitepaper generation complete"
 #cd ..
 
+build_sass () {
+    sass templates/style/scss/custom-bootstrap.scss build/css/custom-bootstrap.css
+    echo "SASS compilation complete"
+}
 
-
+build_pug () {
+    pug templates/routes/pug -o templates/routes/ 
+}
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -s|--sass) 
-        sass templates/style/scss/custom-bootstrap.scss build/css/custom-bootstrap.css
-        echo "SASS compilation complete"
-        shift ;;
+            SASS=true
+            shift ;;
         -p|--pug) 
-        pug templates/routes/pug -o templates/routes/ 
-        shift ;;
-
+            PUG=true
+            shift ;;
+        --init)
+            SASS=true
+            PUG=true
+            shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
 done
-        
+
+test $PUG && build_pug
+test $SASS && build_sass
+
    
 if [ -z "`raco pkg show | grep yaml`" ]
 then
--- generate.rkt
+++ generate.rkt
@@ -16,42 +16,32 @@
          (display (include-template , (string-append
                                        "templates/routes/" (string-replace fname ".html" ".src.html"))))))))
 
-;;; (define-macro (generate-all fname)
-;;;   `(let ()
-;;;      (generate ,fname)
-;;;     ;;;  (let ([LANG "zh-CN"])
-;;;     ;;;    (generate ,fname))
-;;;     ;;;  (let ([LANG "zh-HK"])
-;;;     ;;;    (generate ,fname))))
-;;;   ))
-;;; (define LANG "en-US")
+(define-macro (generate-all fname)
+  `(let ()
+     (generate ,fname)
+    ;;;  (let ([LANG "zh-CN"])
+    ;;;    (generate ,fname))
+    ;;;  (let ([LANG "zh-HK"])
+    ;;;    (generate ,fname))))
+  ))
+(define LANG "en-US")
 
-;;; (define-macro (l10n str)
-;;;   `(l10n-lookup ,str LANG))
+(define-macro (l10n str)
+  `(l10n-lookup ,str LANG))
 
-;;; (define-macro (l10n-url str)
-;;;   (define lang-suffix (gensym 'lsuf))
-;;;   `(if (equal? LANG "en-US") ,str
-;;;        (let ([,lang-suffix (string-downcase (string-replace LANG "-" ""))])
-;;;          (string-replace ,str ".html"
-;;;                          (format "-~a.html" ,lang-suffix)))))
+(define-macro (l10n-url str)
+  (define lang-suffix (gensym 'lsuf))
+  `(if (equal? LANG "en-US") ,str
+       (let ([,lang-suffix (string-downcase (string-replace LANG "-" ""))])
+         (string-replace ,str ".html"
+                         (format "-~a.html" ,lang-suffix)))))
 
-;;; (define-macro (l10n-url-foreign str lang)
-;;;   `(let ([LANG ,lang])
-;;;      (l10n-url ,str)))
-
-<<<<<<< HEAD
-;;; (generate-all "learn.html")
-;;; ;;; (generate-all "resources.html")
-;;; ;;; (generate-all "roadmap.html")
-;;; (displayln "HTML compilation complete") 
-=======
 (define-macro (l10n-url-foreign str lang)
   `(let ([LANG ,lang])
      (l10n-url ,str)))
 
-(generate-all "learn.html")
+;;; (displayln "HTML compilation...")
+(generate-all "learn2.html")
 ;;; (generate-all "resources.html")
 ;;; (generate-all "roadmap.html")
 (displayln "HTML compilation complete")
->>>>>>> d86faaaff8215466d2fd256c24a0ff1bf02eb039
