8ecb8ba8971e75d60c8c2e8d0d857249975d8c21
Shane Mendez <mendezsoftware@gmail.com>
2022-01-06 16:35:59 -0600

    DONE

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-git subtree push -P build sshmendez gh-pages;
+#git subtree push -P build sshmendez gh-pages;
+git push sshmendez `git subtree split --prefix build shane/dev`:gh-pages --force
+
